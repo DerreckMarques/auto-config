22565de59c7f486acb35f3cc4455cf96561a76a6
Derreck <derreck775@gmail.com>
2023-01-13 10:01:02 -0300

    organizando o banco de dados

---
+++ ext/db/db.py
@@ -0,0 +1,21 @@
+#executar esse script na linha de comando para inicializar o banco de dados
+import sqlite3
+
+connection = sqlite3.connect('database.db')
+
+
+with open('schema.sql') as f:
+    connection.executescript(f.read())
+
+cur = connection.cursor()
+
+cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
+            ('First Post', 'Content for the first post')
+            )
+
+cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
+            ('Second Post', 'Content for the second post')
+            )
+
+connection.commit()
+connection.close()
